173526521f431665a3dc184f6767dab37b8eca5c
Marat Faskhiev <marat.faskhiev@gmail.com>
2023-04-30 20:43:11 +0500

    feat: protobuf serialization

---
+++ src/OutboxFlow/Serialization/ProducePipelineExtensions.Protobuf.cs
@@ -0,0 +1,39 @@
+﻿using Google.Protobuf;
+
+namespace OutboxFlow.Serialization;
+
+public static partial class ProducePipelineExtensions
+{
+    /// <summary>
+    /// Serialize a message to a byte array in protobuf encoding.
+    /// </summary>
+    /// <param name="pipeline">Pipeline.</param>
+    /// <typeparam name="T">Pipeline input message type.</typeparam>
+    public static ProducePipelineStep<T, T> SerializeToProtobuf<T>(
+        this ProducePipeline<T> pipeline) where T : IMessage
+    {
+        return pipeline.AddStep((message, context) =>
+        {
+            context.Value = message.ToByteArray();
+            return new ValueTask<T>(message);
+        });
+    }
+
+    /// <summary>
+    /// Serialize a message key to a byte array in protobuf encoding.
+    /// </summary>
+    /// <param name="pipeline">Pipeline.</param>
+    /// <param name="keyProvider">Provides a key value to serialize.</param>
+    /// <typeparam name="T">Pipeline input message type.</typeparam>
+    /// <typeparam name="TKey">Message key type.</typeparam>
+    public static ProducePipelineStep<T, T> SerializeKeyToProtobuf<T, TKey>(
+        this ProducePipeline<T> pipeline, Func<T, TKey> keyProvider)
+        where TKey : IMessage
+    {
+        return pipeline.AddStep((message, context) =>
+        {
+            context.Key = keyProvider(message).ToByteArray();
+            return new ValueTask<T>(message);
+        });
+    }
+}
---
+++ src/OutboxFlow/Serialization/ProducePipelineStepExtensions.Protobuf.cs
@@ -0,0 +1,41 @@
+﻿using Google.Protobuf;
+
+namespace OutboxFlow.Serialization;
+
+public static partial class ProducePipelineStepExtensions
+{
+    /// <summary>
+    /// Serialize a message to a byte array in protobuf encoding.
+    /// </summary>
+    /// <param name="step">Step.</param>
+    /// <typeparam name="TIn">Step input message type.</typeparam>
+    /// <typeparam name="TOut">Step output message type.</typeparam>
+    public static ProducePipelineStep<TOut, TOut> SerializeToProtobuf<TIn, TOut>(
+        this ProducePipelineStep<TIn, TOut> step) where TOut : IMessage
+    {
+        return step.AddStep((message, context) =>
+        {
+            context.Value = message.ToByteArray();
+            return new ValueTask<TOut>(message);
+        });
+    }
+
+    /// <summary>
+    /// Serialize a message key to a byte array in protobuf encoding.
+    /// </summary>
+    /// <param name="step">Step.</param>
+    /// <param name="keyProvider">Provides a key value to serialize.</param>
+    /// <typeparam name="TIn">Step input message type.</typeparam>
+    /// <typeparam name="TOut">Step output message type.</typeparam>
+    /// <typeparam name="TKey">Message key type.</typeparam>
+    public static ProducePipelineStep<TOut, TOut> SerializeKeyToProtobuf<TIn, TOut, TKey>(
+        this ProducePipelineStep<TIn, TOut> step, Func<TOut, TKey> keyProvider)
+        where TKey : IMessage
+    {
+        return step.AddStep((message, context) =>
+        {
+            context.Key = keyProvider(message).ToByteArray();
+            return new ValueTask<TOut>(message);
+        });
+    }
+}
