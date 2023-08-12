47207941f39348e4cfa2cf9ba7888088e9ef8706
Marat Faskhiev <marat.faskhiev@gmail.com>
2023-08-12 21:15:34 +0500

    test: ConsumerBuilderExtensions tests

---
+++ tests/OutboxFlow.Postgres.UnitTests/ConsumerBuilderExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using OutboxFlow.Consume.Configuration;
+using Xunit;
+
+namespace OutboxFlow.Postgres.UnitTests;
+
+public sealed class ConsumerBuilderExtensionsTests : IDisposable
+{
+    private readonly Mock<IConsumerBuilder> _builder = new(MockBehavior.Strict);
+
+    public void Dispose()
+    {
+        Mock.VerifyAll(_builder);
+    }
+
+    [Fact]
+    public void UsePostgres_SetsOutboxStorageRegistrar()
+    {
+        _builder.SetupSet(x => x.OutboxStorageRegistrar = It.IsAny<ConsumerOutboxStorageRegistrar>());
+
+        _builder.Object.UsePostgres("connectionString");
+
+        _builder.VerifySet(x => x.OutboxStorageRegistrar = It.IsAny<ConsumerOutboxStorageRegistrar>(), Times.Once);
+    }
+}
