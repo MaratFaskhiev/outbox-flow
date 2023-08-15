e10d292fac1a23c9352ba44d83060d034d9c29dc
Marat Faskhiev <marat.faskhiev@gmail.com>
2023-08-15 21:45:13 +0500

    test: first integration test

---
+++ tests/OutboxFlow.Postgres.IntegrationTests/DatabaseFixture.cs
@@ -0,0 +1,23 @@
+﻿using Testcontainers.PostgreSql;
+using Xunit;
+
+namespace OutboxFlow.Postgres.IntegrationTests;
+
+public sealed class DatabaseFixture : IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder()
+        .WithResourceMapping("postgres.sql", "/docker-entrypoint-initdb.d")
+        .Build();
+
+    public string ConnectionString => _postgreSqlContainer.GetConnectionString();
+
+    public Task InitializeAsync()
+    {
+        return _postgreSqlContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _postgreSqlContainer.DisposeAsync().AsTask();
+    }
+}
---
+++ tests/OutboxFlow.Postgres.IntegrationTests/OutboxLockManagerTests.cs
@@ -0,0 +1,28 @@
+﻿using Npgsql;
+using Xunit;
+
+namespace OutboxFlow.Postgres.IntegrationTests;
+
+public sealed class OutboxLockManagerTests : IClassFixture<DatabaseFixture>
+{
+    private readonly string _connectionString;
+
+    private readonly OutboxLockManager _manager = new();
+
+    public OutboxLockManagerTests(DatabaseFixture databaseFixture)
+    {
+        _connectionString = databaseFixture.ConnectionString;
+    }
+
+    [Fact]
+    public async Task LockAsync_NoLock_ReturnsLockRecord()
+    {
+        await using var connection = new NpgsqlConnection(_connectionString);
+        await connection.OpenAsync();
+        await using var transaction = await connection.BeginTransactionAsync();
+
+        var outboxLock = await _manager.LockAsync(TimeSpan.FromMinutes(5), transaction, CancellationToken.None);
+
+        Assert.NotNull(outboxLock);
+    }
+}
