49309d544168dc69bbf9ba9437f8ab9bd1240b99
Marat Faskhiev <marat.faskhiev@gmail.com>
2023-08-16 22:03:04 +0500

    test: OutboxFlow.Kafka integration tests (#8)

---
+++ tests/OutboxFlow.Kafka.IntegrationTests/KafkaFixture.cs
@@ -0,0 +1,21 @@
+﻿using Testcontainers.Kafka;
+using Xunit;
+
+namespace OutboxFlow.Kafka.IntegrationTests;
+
+public sealed class KafkaFixture : IAsyncLifetime
+{
+    private readonly KafkaContainer _kafkaContainer = new KafkaBuilder().Build();
+
+    public string BootstrapAddress => _kafkaContainer.GetBootstrapAddress();
+
+    public Task InitializeAsync()
+    {
+        return _kafkaContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _kafkaContainer.DisposeAsync().AsTask();
+    }
+}
---
+++ tests/OutboxFlow.Kafka.IntegrationTests/KafkaProducerBuilderTests.cs
@@ -0,0 +1,51 @@
+﻿using Confluent.Kafka;
+using Xunit;
+
+namespace OutboxFlow.Kafka.IntegrationTests;
+
+public sealed class KafkaProducerBuilderTests : IClassFixture<KafkaFixture>
+{
+    private const string Topic = "test_topic";
+    private readonly string _bootstrapAddress;
+
+    private readonly KafkaProducerBuilder _builder = new();
+
+    public KafkaProducerBuilderTests(KafkaFixture kafkaFixture)
+    {
+        _bootstrapAddress = kafkaFixture.BootstrapAddress;
+    }
+
+    [Fact]
+    public async Task ProduceMessageTest()
+    {
+        var key = Guid.NewGuid();
+        var value = Guid.NewGuid();
+
+        using var producer = _builder.Create(new ProducerConfig
+        {
+            BootstrapServers = _bootstrapAddress
+        });
+        var kafkaMessage = new Message<byte[], byte[]>
+        {
+            Key = key.ToByteArray(),
+            Value = value.ToByteArray()
+        };
+        var result = await producer.ProduceAsync(Topic, kafkaMessage);
+
+        Assert.Equal(PersistenceStatus.Persisted, result.Status);
+
+        using var consumer = new ConsumerBuilder<byte[], byte[]>(new ConsumerConfig
+        {
+            BootstrapServers = _bootstrapAddress,
+            GroupId = "test_consumer",
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            EnablePartitionEof = true
+        }).Build();
+        consumer.Subscribe(Topic);
+        var consumeResult = consumer.Consume(TimeSpan.FromSeconds(3));
+
+        Assert.NotNull(consumeResult);
+        Assert.Equal(key, new Guid(consumeResult.Message.Key));
+        Assert.Equal(value, new Guid(consumeResult.Message.Value));
+    }
+}
